950e76f68550188538e8e04a027d5d078c8610fc
geonheeko <geonheeko@172.16.6.204>
2014-11-26 13:39:55 -0500

    s

---
+++ CSC/src/FinalProject/Product.java
@@ -0,0 +1,50 @@
+package FinalProject;
+
+public class Product {
+private String name;
+private double price; 
+
+public Product()
+{
+	name = "";
+	price =0;
+}
+public Product(String name, double price)
+{
+	this.name = name;
+	this.price=price;
+}
+
+public void setName(String name)
+{
+	this.name = name;
+}
+
+public void setPrice(double price)
+{
+	this.price = price;
+}
+
+public String getName()
+{
+	return name;
+}
+public double getPrice()
+{
+	return price;
+}
+
+public boolean equals(Product other)
+{
+	return (this.name.equals(other.getName()) && this.price == other.getPrice());
+}
+
+public String toString()
+{
+	String output = "";
+	output = output +name+ " $" + price;
+	return output;
+}
+
+
+}
